08a0cf1dd577865d09e8b71a6005109279032c9a
Raymond-Cox <raymondc1911@gmail.com>
2025-01-29 16:39:01 -0500

    Proof of concept, send it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import './App.module.css'
+import { useState } from 'react'
+import * as classes from './App.module.css'
 
 /**
  * Main component of the application.
  */
 const App = () => {
-  return <h1>Hello world!</h1>
+  const [imgs, setImgs] = useState<string[]>([])
+
+  const onChange = (event) => {
+    const file = event.target.files[0]
+    const src = URL.createObjectURL(file)
+    setImgs((prev) => [...prev, src])
+
+    const fileElement = document.getElementById('file') as HTMLInputElement
+    fileElement.value = ''
+  }
+
+  const removeImage = (index) => {
+    setImgs((prev) => prev.filter((_, i) => i !== index))
+  }
+
+  return (
+    <div className={classes.container}>
+      <input
+        type="file"
+        accept="image/*"
+        capture="environment"
+        id="file"
+        onChange={onChange}
+      />
+      <div className={classes.imageContainer}>
+        {imgs.map((img, index) => (
+          <div key={img}>
+            <img src={img} alt="captured" />
+            <button onClick={() => removeImage(index)}>Remove</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
